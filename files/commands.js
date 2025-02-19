dfc2c017790a019e20c6de86ddb31991265696a1
Chris Haslam <ch@chrishaslam.co.uk>
2025-02-19 16:25:32 +0000

    new site

---
+++ files/commands.js
@@ -0,0 +1,61 @@
+var twitter = "https://www.twitter.com/chrishasl/";
+var password = "letmein";
+var linkedin = "https://www.linkedin.com/in/chrishaslam/";
+var instagram = "https://www.instagram.com/chrishasl/";
+var github = "https://github.com/chrishasl/";
+var email = 'mailto:?@chrishaslam.co.uk';
+
+whoami = [
+  "Hi, I'm Chris",
+  "I've been in Open Source for 2.5 decades",
+  "During a software engineering degree I built and exited Security Forums",
+  "After I founded and grew the agency Ixis to a team of 20, that was recognised by the cabinet office and listed in the Deloitte Fast 500",
+  "I now provide consultancy for open source and digital via <a href=https://exitzero.co.uk>exit(0) digital</a>",
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/chrishaslam' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/chrishasl' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/chrishasl' + '</a>',
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/chrishasl' + '</a>',
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Be careful hacking the gibson',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "MOAR SOON.",
+  "<br>"
+];
+
+
+help = [
+  "<br>",
+  '<span class="command">whoami</span>         Who am I?',
+  '<span class="command">social</span>         Display socials',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">secret</span>         Find the password',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">ChrisHaslam.co.uk</span>',
+  "<br>",
+  "  oooooooo8 oooo                   o88                    ooooo ooooo                       o888                           ",
+  "o888     88  888ooooo   oo oooooo  oooo   oooooooo8        888   888   ooooooo    oooooooo8  888   ooooooo   oo ooo oooo   ",
+  "888          888   888   888    888 888  888ooooooo        888ooo888   ooooo888  888ooooooo  888   ooooo888   888 888 888  ",
+  "888o    oo   888   888   888        888          888       888   888 888    888          888 888 888    888   888 888 888  ",
+  "888oooo88   o888o o888o o888o      o888o 88oooooo88       o888o o888o 88ooo88 8o 88oooooo88 o888o 88ooo88 8o o888o888o888o  ",
+  "<br>",
+                                                                                                                           
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
